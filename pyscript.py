fcfc7ca784bedd482e7f12d0872b7cfe6e634d2f
Ruthvik Ravindra <ruthvikravindra1997@gmail.com>
2022-04-12 23:17:32 -0700

    Update pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -13,14 +13,14 @@ headers = {
 run_id = os.environ['GITHUB_RUN_ID']
 
 try:
-    job_list = requests.get(f'https://www.github.com/api/v3/repos/ruthvik17/aws-otel-collector/actions/runs/{run_id}/jobs', headers=headers)
+    response = requests.get(f'https://github.com/api/v3/repos/ruthvik17/aws-otel-collector/actions/runs/{run_id}/jobs', headers=headers)
 except requests.exceptions.ConnectionError:
     print("Connection refused. Sleeping.......")
     sleep(0.5)
 
 # job_list = requests.get(f'https://HOSTNAME/api/v3/repos/ruthvik17/aws-otel-collector/actions/runs/{run_id}/jobs', headers=headers)
 
-print(job_list)
+print(response)
 
 
 # # the file to be converted to 
@@ -38,51 +38,51 @@ print(job_list)
 
 
 # fields in the sample file 
-fields =['job_name', 'curent_job', 'log']
+# fields =['job_name', 'curent_job', 'log']
 
-# p = 0
-# creating dictionary
-with open(filename) as fh:
-    line_no = 1
-    for line in fh:
-        dict1 = {}
+# # p = 0
+# # creating dictionary
+# with open(filename) as fh:
+#     line_no = 1
+#     for line in fh:
+#         dict1 = {}
 
-        id_dict = { "_index": "logs", "_id" : str(line_no + 1) }
-        dict1["index"] = id_dict
+#         id_dict = { "_index": "logs", "_id" : str(line_no + 1) }
+#         dict1["index"] = id_dict
         
-        # reads each line and trims of  the extra spaces 
-        # and gives only the valid words
-        description = list( line.strip().split('\t', 3))
-        # print(description)
-        # loop variable
+#         # reads each line and trims of  the extra spaces 
+#         # and gives only the valid words
+#         description = list( line.strip().split('\t', 3))
+#         # print(description)
+#         # loop variable
 
-        if len(description) < len(fields):
-            # dict2[line_no] = description
-            # line_no += 1
-            continue
+#         if len(description) < len(fields):
+#             # dict2[line_no] = description
+#             # line_no += 1
+#             continue
         
-        i = 0
-        # intermediate dictionary
-        dict2 = {}
-        while i<len(fields):
+#         i = 0
+#         # intermediate dictionary
+#         dict2 = {}
+#         while i<len(fields):
               
-                # creating dictionary for each employee
-                dict2[fields[i]]= description[i]
-                i = i + 1
+#                 # creating dictionary for each employee
+#                 dict2[fields[i]]= description[i]
+#                 i = i + 1
                   
-        # appending the record of each employee to
-        # the main dictionary
+#         # appending the record of each employee to
+#         # the main dictionary
         
-        # print(dict1)
-        line_no += 1
+#         # print(dict1)
+#         line_no += 1
 
 
-        data = [ dict1, dict2]
+#         data = [ dict1, dict2]
 
 
-        with open('test_json_py.json', 'a') as fp:
-            fp.write(
-            '\n'.join(json.dumps(i) for i in data) + '\n')
+#         with open('test_json_py.json', 'a') as fp:
+#             fp.write(
+#             '\n'.join(json.dumps(i) for i in data) + '\n')
 
 
-print('Uploaded the data')
+# print('Uploaded the data')
