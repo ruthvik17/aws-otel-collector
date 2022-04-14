eb803f42f638c197cc5075c5bfc300c6c68dce72
Ruthvik Ravindra <ruthvikravindra1997@gmail.com>
2022-04-14 15:51:47 -0700

    Update pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -11,49 +11,21 @@ headers = {
 run_id = os.environ['GITHUB_RUN_ID']
 repo = os.environ['GITHUB_REPOSITORY']
 
+# Look at the workflow data
 
 try:
-    job_list = requests.get(f'https://api.github.com/repos/{repo}/actions/runs/{run_id}/jobs', headers=headers)
-except requests.exceptions.ConnectionError:
-    print("Connection refused. Sleeping.......")
-    sleep(0.5)
-
-# job_list = requests.get(f'https://HOSTNAME/api/v3/repos/ruthvik17/aws-otel-collector/actions/runs/{run_id}/jobs', headers=headers)
-
-# print(job_list.json())
-
-job_data = job_list.json()
-
-#Get Job Id
-job_id = job_data['jobs'][0]['id']
-
-# Look at the work
-
-headers = {
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': access_token
-}
-
-try:
-    job_logs= requests.get(f'https://api.github.com/repos/{repo}/actions/jobs/{job_id}/logs', headers=headers)
-except requests.exceptions.ConnectionError:
-    print("Connection refused. Sleeping.......")
-    sleep(0.5)
-    
- try:
-    job_list = requests.get('https://api.github.com/repos/ruthvik17/aws-otel-collector/actions/runs/2168746686/jobs', headers=headers)
+    job_data = requests.get(f'https://api.github.com/repos/{repo}/actions/runs/{run_id}/jobs', headers=headers)
 except requests.exceptions.ConnectionError:
     print("Connection refused. Sleeping.......")
     sleep(0.5)
 
 job_data = job_list.json()
-
-print("\nTotal count of jobs", len(job_data["jobs"]))
-
-job_id = job_data["jobs"][0]["id"]
 
 keys = list(job_data["jobs"][0].keys())
+
 id_no = 0
+
+# Loop through jobs data
 for job in job_data["jobs"]:
     id_no += 1
     dict1 = {}
@@ -74,6 +46,7 @@ for job in job_data["jobs"]:
         fp.write(
         '\n'.join(json.dumps(dict) for dict in dicts) + '\n')
 
+# Push to data table on OpenSearch
 headers = {
     'Content-Type': 'application/json',
 }
@@ -81,9 +54,10 @@ headers = {
 with open('meta_json.json', 'rb') as f:
     data = f.read()
 
-response = requests.post('https://search-gh-test-mn2dq77arhyercpvg3sgdihpnq.us-west-2.es.amazonaws.com/_bulk', headers=headers, data=data, auth=('ruthvik', 'Ruthvik-19'))
+response = requests.post('https://search-gh-test-mn2dq77arhyercpvg3sgdihpnq.us-west-2.es.amazonaws.com/_bulk', 
+                         headers=headers, data=data, auth=('ruthvik', 'Ruthvik-19'))
 
-print("OPensearch response \n ", response.json())
+# print("OPensearch response \n ", response.json())
 
 print('\nUploaded to the data table')
 
