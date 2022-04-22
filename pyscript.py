a155ed19c572b9fe6951615599261226c9beef06
Ruthvik Ravindra <ruthvikravindra1997@gmail.com>
2022-04-22 09:05:01 -0700

    Update pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -124,7 +124,7 @@ region = 'us-west-2' # e.g. us-west-1
 awsauth = AWSV4SignerAuth(credentials, region)
 OS_endpt = os.environ['OS_ENDPOINT'] 
 
-with open('meta_json_data.json', 'rb') as f:
+with open('meta_json_logs.json', 'rb') as f:
     data = f.read()
 
 response = requests.post(f'{OS_endpt}/_bulk', 
